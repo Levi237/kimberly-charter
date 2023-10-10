2fe3c494edcdd60bb0d48faf26cfba93f9ff8f62
Levi Winans <levieiko@gmail.com>
2023-10-10 14:13:58 -0700

    start websute

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import HomePage from './pages/home';
 
-export default App;
+export default class App extends Component {
+  render (){
+    return (
+      <div className="body">
+        <header><img className="name-logo" width="50%" height="auto" src="name_logo.png" alt="Kimberly Charter - name logo"/></header>
+        <HomePage />
+      </div>
+    );
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import firebase from "firebase/compat/app";
+// import 'firebase/firestore';
+ 
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// firebase.initializeApp({
+//   apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+//   authDomain: "kimberly-charter.firebaseapp.com",
+//   projectId: "kimberly-charter",
+//   storageBucket: "kimberly-charter.appspot.com",
+//   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+//   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+//   measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
+// });
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
