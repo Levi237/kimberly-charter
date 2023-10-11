f627dafb63d971b1019ddbc8f0254e66f66c182b
Levi Winans <levieiko@gmail.com>
2023-10-11 12:36:34 -0700

    add footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
       <div className="body">
         <header><img className="name-logo" width="50%" height="auto" src="name_logo.png" alt="Kimberly Charter - name logo"/></header>
         <HomePage />
+        <footer></footer>
       </div>
     );
   };
