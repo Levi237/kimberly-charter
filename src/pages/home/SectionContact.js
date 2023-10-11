14511df055d7e428c05f96e866da27de6703a2ac
Levi Winans <levieiko@gmail.com>
2023-10-11 14:54:56 -0700

    update nomenclature

--- src/pages/home/SectionContact.js
+++ src/pages/home/SectionContact.js
@@ -17,12 +17,7 @@ const ContactSection = () => {
     // }, []);
 
     return(
-        <section ref={ref} className="home-section-a hide-item item page-width">
-            <img className="headshot desktop-only" width="50%" src="Pastor_Kim_Portrait_tall.jpg" alt="Kim Headshot"/>
-            <p>In a world where we feel disconnected and time is so valuable, how do we find the path for healing and wholeness?</p>
-            <img className="headshot mobile-only" width="50%" src="Pastor_Kim_Portrait_tall.jpg" alt="Kim Headshot"/>
-            <p>Kimberly has brought the world of fellowship and friendship to a new platform, allowing you to be present from your computer for 60 to 90 minutes once a week with a small group of likeminded people who want to improve their lives with connectivity.</p>
-            <p>Kimberly brings a level of spirituality and caring to the table that helps you to know you are in a safe place, with quite possibly the opportunity to make new friends along the way, who may make a difference in your life while growing and healing are always the goal.</p>
+        <section ref={ref} className="home-contact hide-item item page-width">
         </section>
     );
 };
