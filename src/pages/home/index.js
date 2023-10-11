9e89933bb8128f04b5acca93dcc9361b3158cd68
Levi Winans <levieiko@gmail.com>
2023-10-11 12:36:19 -0700

    add more intersectionOb text

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -16,18 +16,24 @@ export default class HomePageextends extends Component {
                         <TextiOSnippet>
                             <h3><center>Let's get to know each other and heal & grow together.</center></h3>   
                         </TextiOSnippet>
-                        <br/>                     
-                        <p><center>This is a guided conversation, where you will have an opportunity to discuss with the group as Kimberly moderates the conversation</center></p>
+                        <br/>    
+                        <TextiOSnippet>             
+                            <p><center>This is a guided conversation, where you will have an opportunity to discuss with the group as Kimberly moderates the conversation</center></p>
+                        </TextiOSnippet>    
                        <br/>
-                       <p>Our circle of friends time will be an online zoom meeting with no more than 10, so that everyone can have an opportunity to participate.  The class will be a 6 week session.  Once you register you will receive a Zoom link and can participate from the comfort of wherever you are most comfortable.</p>
-                        <br/>
+                       <TextiOSnippet>    
+                        <p>Our circle of friends time will be an online zoom meeting with no more than 10, so that everyone can have an opportunity to participate.  The class will be a 6 week session.  Once you register you will receive a Zoom link and can participate from the comfort of wherever you are most comfortable.</p>
+                       </TextiOSnippet>    
+                    <br/>
                         <h4>Example topics</h4>
                     <ul>
                         <li>How do I live a life of faith</li>
                         <li>Good Grief…How did I get here?</li>
                         <li>Why am I having to recreate myself?</li>
                     </ul>
-                    <p>These are just a sample of the topics we will discuss in a session.</p>
+                    <TextiOSnippet>  
+                        <p>These are just a sample of the topics we will discuss in a session.</p>
+                    </TextiOSnippet>
                     </div>
                 </div>
 
