5fc2cae0409a9de23a0851d15587f295dcb6d235
Levi Winans <levieiko@gmail.com>
2023-10-13 12:00:49 -0700

    update header, add logo, move to own file

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header>
+            <img className="name-logo" width="50%" height="auto" src="kimberly-charter-ministries_stacked-logo_1.png" alt="Kimberly Charter Ministries"/>
+        </header>
+    );
+};
+
+export default Header;
